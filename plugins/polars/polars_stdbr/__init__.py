bec73359383bb49fb5649f640d2232010a9f7dcd
fullzer4 <gabrielpelizzaro@gmail.com>
2026-05-11 00:06:20 -0300

    feat: add Polars expression plugin for CPF, CNPJ, CEP
    Adds a new plugin under plugins/polars that exposes stdbr-core
    validation, formatting and symbol removal as native Polars expressions.
    
    The plugin compiles as a cdylib loaded directly by the Polars engine,
    operating on Arrow buffers without GIL overhead or data copying.
    
    Usage:
      import polars_stdbr
      df.with_columns(pl.col("cpf").stdbr.cpf_is_valid())
    
    Expressions: cpf_is_valid, cpf_format, cpf_remove_symbols,
    cnpj_is_valid, cnpj_format, cnpj_remove_symbols,
    cep_is_valid, cep_format, cep_remove_symbols.

---
+++ plugins/polars/polars_stdbr/__init__.py
@@ -0,0 +1,25 @@
+from polars_stdbr._expressions import (
+    StdbrExpr,
+    cep_format,
+    cep_is_valid,
+    cep_remove_symbols,
+    cnpj_format,
+    cnpj_is_valid,
+    cnpj_remove_symbols,
+    cpf_format,
+    cpf_is_valid,
+    cpf_remove_symbols,
+)
+
+__all__ = [
+    "StdbrExpr",
+    "cep_format",
+    "cep_is_valid",
+    "cep_remove_symbols",
+    "cnpj_format",
+    "cnpj_is_valid",
+    "cnpj_remove_symbols",
+    "cpf_format",
+    "cpf_is_valid",
+    "cpf_remove_symbols",
+]
